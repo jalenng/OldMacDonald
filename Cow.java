52644cfbc0916aec81b07856e7a9252857282f34
jalenng <jang2@s.sfusd.edu>
2018-10-16 13:38:27 -0700

    did farm and cow

--- Cow.java
+++ Cow.java
@@ -4,6 +4,20 @@ class Cow implements Animal
      private String mySound;
      public Cow(String type, String sound)
      {
-     	
+     	myType = type;
+     	mySound = sound;
+     }
+     public Cow()
+     {
+     	myType = "unknown";
+     	mySound = "unknown";
+     }
+     public String getSound()
+     {
+     	return mySound;
+     }
+     public String getType()
+     {
+     	return myType;
      }
 }
